941edb196560b1496165b0b51d282fcee60042df
neil patel <neil@myindianweddingapp.com>
2019-03-25 10:40:59 +0000

    Added function to fade in on scroll

---
+++ scroll.js
@@ -0,0 +1,11 @@
+$(document).on("scroll", function() {
+    var scrollTop = $(document).scrollTop()
+    var scrollBottom = scrollTop + $(window).height()
+
+    var pageBottom = $(document).height()
+
+    var diff = pageBottom - scrollBottom
+    var opacity = 1 - diff / 300
+
+    $("div.fade-bg").css("opacity", opacity)
+})
